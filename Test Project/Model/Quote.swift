56ed2b00d9c99fdf5efb6fa73a72ac1bad1f3bcf
cloudiosx <cloudiosx@gmail.com>
2022-11-03 16:41:09 -0400

    Added model for Quotes

---
+++ Test Project/Model/Quote.swift
@@ -0,0 +1,14 @@
+//
+//  Quote.swift
+//  Test Project
+//
+//  Created by John Kim on 11/3/22.
+//
+
+import Foundation
+
+struct Quote: Codable {
+    let quote: String
+    let author: String
+    let slideTransitionDelay: Int
+}
